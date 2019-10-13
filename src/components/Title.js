146e5cbde4468ac36e2e637ca2e2ee0db1d1e099
Agata Musiuk <agata.musiuk@gmail.com>
2019-10-13 11:00:30 +0200

    Update Title props

--- src/components/Title.js
+++ src/components/Title.js
@@ -4,14 +4,13 @@ import style from './Title.css';
 class Title extends React.Component {
     constructor(props) {
         super(props);
-
     }
 
     render() {
         return (
             <div className={style.Title}>
                 <h1>My to do list app</h1>
-                <h4>You have now {} tasks on your to do list</h4>
+                <h4>You have now {this.props.count} tasks on your to do list</h4>
             </div>
         );
     }
--- src/containers/App.js
+++ src/containers/App.js
@@ -31,7 +31,7 @@ class App extends React.Component {
     render() {
         return(
             <div className={style.TodoApp}>
-                <Title />
+                <Title count={this.state.data.length}/>
             </div>
         );
     }
